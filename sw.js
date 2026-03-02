79618b8dedecd2a22ef373f0c4e724ead686befd
gilenogestorfinanceiro <gilenopaivalima@gmail.com>
2026-03-02 15:42:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Gileno - Gestão Financeira | Service Worker
 // Atualizar este número a cada nova versão do app
-const CACHE_VERSION = '2.9.1';
+const CACHE_VERSION = '2.9.2';
 const CACHE_NAME = 'gileno-gf-' + CACHE_VERSION;
 
 const FILES_TO_CACHE = [
